890242d935d60f6a756e77a5c675c7d9f980eece
Chase Gladish <chase.gladish@gmail.com>
2022-10-01 14:41:44 -0700

    Add auth0 stuff

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import { auth } from "express-openid-connect";
 import prisma from "./prisma";
 import { Request } from "./types";
 import apps from "./routes/apps";
 import snippets from "./routes/snippets";
 import rateLimit from "./rate-limit";
+
 dotenv.config();
 
 const app: Express = express();
@@ -17,6 +19,18 @@ app.use(
     Number(process.env.RATE_LIMIT_PER_SECOND)
   )
 );
+if (process.env.NODE_ENV !== "development") {
+  app.use(
+    auth({
+      authRequired: false,
+      auth0Logout: true,
+      secret: process.env.AUTH0_CLIENT_SECRET,
+      baseURL: `http://localhost:${port}`,
+      clientID: process.env.AUTH0_CLIENT_ID,
+      issuerBaseURL: process.env.AUTH0_DOMAIN,
+    })
+  );
+}
 app.use((req: Request, res, next) => {
   req.prisma = prisma;
   next();
@@ -27,7 +41,7 @@ app.use("/apps", apps);
 app.use("/snippets", snippets);
 
 app.get("/", (req: Request, res) => {
-  res.sendStatus(200);
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
 });
 
 if (!process.env.JEST_WORKER_ID) {
