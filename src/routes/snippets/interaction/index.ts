86ccfe0a7528e18beaa438f4ad930a37aaf32f6f
Chase Gladish <chase.gladish@gmail.com>
2022-10-04 12:22:48 -0700

    View count stuff

--- src/routes/snippets/interaction/index.ts
+++ src/routes/snippets/interaction/index.ts
@@ -40,11 +40,30 @@ router.get(
       if (!snippetInteraction) {
         return res.sendStatus(404);
       }
-      res
-        .status(200)
-        .json(
-          snippetInteraction && entityToType(req.prisma, snippetInteraction)
-        );
+      res.status(200).json(entityToType(req.prisma, snippetInteraction));
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.post(
+  "/",
+  async (
+    req: Request<{ snippetId: string }>,
+    res: Response<ExternalSnippetInteraction | null>,
+    next
+  ) => {
+    try {
+      await req.prisma.snippetInteraction.update({
+        where: {
+          snippetId: req.prisma.snippet.externalIdToId(req.params.snippetId),
+        },
+        data: {
+          views: { increment: 1 },
+        },
+      });
+      res.status(200);
     } catch (err) {
       next(err);
     }
